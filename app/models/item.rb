81b87b5b4b2f73fd5f47c7a6ee7c47a9042e1577
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-21 19:00:45 +0300

    Add name validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
+
+  validates :name, presence: true, allow_blank: false
 end

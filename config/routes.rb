b88994e1115a0b1495422077ad83d6811d5c44d9
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-21 14:17:17 +0300

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
-      resources :items, only: [:index, :show, :create, :update, :destroy]
-      resources :reservations, only: [:index, :show, :create]
+      resources :items
+      resources :users
+      resources :reservations
     end
   end
 end

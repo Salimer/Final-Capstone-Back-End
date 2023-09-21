41b5909a0831b31e6a132555725d57ab66551a35
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-21 14:14:50 +0300

    Add models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :items, through: :reservation
 end

19fd269c55118aa0b1ad012934d32a8d9297d368
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-30 01:27:40 +0300

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
+  has_one_attached :image
 
   validates :name, presence: true, allow_blank: false
 end

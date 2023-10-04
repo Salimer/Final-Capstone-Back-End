3cdfa81b8e38f54c494e281db778258a35618bfb
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-10-04 04:45:19 -0600

    Add application.routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.routes.default_url_options[:host] = 'https://harleydavidson.onrender.com'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
